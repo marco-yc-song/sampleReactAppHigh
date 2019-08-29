4a6253b505260e2b0413894f3b11e0586b137a73
jamsya <jamsya@naver.com>
2019-08-29 20:36:07 +0900

    1.3 {} 이용한 객체 분해

--- src/index.js
+++ src/index.js
@@ -5,7 +5,38 @@ const button = document.querySelector('.btn1');
 button.addEventListener("click", (event) => console.log(event));
 
 //default value = Human
-const sayHello = (name = "Human") => "Hello " + name;
+const sayHello = (name = "Human") => `Hello ${name}`;
 const nicolas = sayHello('Nicolas');
-
 console.log(nicolas);
+
+//1.3 Object
+const human = {
+    name:"Nike",
+    lastName:"Boko",
+    nationality:"Korean",
+
+    favFood:{
+
+        breakfast:"Burger",
+        lunch:"Kimchi",
+        dinner:"Steak"
+
+    }
+
+}
+
+
+//아래 두 코드는 동일하다!
+
+// const name = human.name;
+// const lastName = human.lastName;
+// const difName = human.nationality;
+
+const {
+    name, 
+    lastName, 
+    nationality:difName, 
+    favFood:{dinner,lunch,breakfast} 
+} = human;
+
+console.log(name,lastName, difName, dinner, lunch, breakfast);
